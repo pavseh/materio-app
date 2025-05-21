7f9438098df01316c97085bc52a95ba20250e937
Ivern <115958709+pavseh@users.noreply.github.com>
2025-05-21 16:23:03 +0800

    Views

--- MaterioApp/apps/dashboards/views.py
+++ MaterioApp/apps/dashboards/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from .models import College, Organization, OrgMember, Program, Student
 from web_project import TemplateLayout
 
 
@@ -8,11 +9,21 @@ Here you can override the page view layout.
 Refer to dashboards/urls.py file for more pages.
 """
 
-
 class DashboardsView(TemplateView):
-    # Predefined function
     def get_context_data(self, **kwargs):
-        # A function to init the global layout. It is defined in web_project/__init__.py file
         context = TemplateLayout.init(self, super().get_context_data(**kwargs))
-
+        context['colleges'] = College.objects.all()
+        context['organizations'] = Organization.objects.all()
+        context['org_members'] = OrgMember.objects.all()
+        context['programs'] = Program.objects.all()
+        context['students'] = Student.objects.all()
         return context
+
+    # old template view - static data
+    # class DashboardsView(TemplateView):
+#     # Predefined function
+#     def get_context_data(self, **kwargs):
+#         # A function to init the global layout. It is defined in web_project/__init__.py file
+#         context = TemplateLayout.init(self, super().get_context_data(**kwargs))
+
+#         return context
